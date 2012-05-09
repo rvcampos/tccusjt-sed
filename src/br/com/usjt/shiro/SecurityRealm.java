33faecd2599ae376c94eba727ad36be36e2e6777
renanvcampos <renanvcampos@gmail.com>
2012-05-09 11:29:19 -0300

    Pagina LOGIN

--- src/br/com/usjt/shiro/SecurityRealm.java
+++ src/br/com/usjt/shiro/SecurityRealm.java
@@ -10,10 +10,7 @@ import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.SimpleAuthorizationInfo;
-import org.apache.shiro.mgt.DefaultSecurityManager;
-import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.realm.AuthorizingRealm;
-import org.apache.shiro.session.mgt.DefaultSessionManager;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.SimplePrincipalCollection;
 import org.hibernate.Criteria;
@@ -31,95 +28,97 @@ import br.com.usjt.util.HS;
 /**
  * Autorizacao do aplicativo usando shiro
  */
-public class SecurityRealm extends AuthorizingRealm {
+public class SecurityRealm extends AuthorizingRealm
+{
 
-	/**
-	 * construtor padrao
-	 */
-	public SecurityRealm() {
-		this.setName(SecurityRealm.class.getName() + "::tccusjt-sed");
-	}
+    /**
+     * construtor padrao
+     */
+    public SecurityRealm()
+    {
+        this.setName(SecurityRealm.class.getName() + "::tccusjt-sed");
+    }
 
-	private static Logger LOG = LoggerFactory.getLogger(SecurityRealm.class);
+    private static Logger LOG = LoggerFactory.getLogger(SecurityRealm.class);
 
-	/**
-	 * Passo 1: Autenticando<br>
-	 * Trazendo o identificador unico do usuario
-	 */
-	@Override
-	protected AuthenticationInfo doGetAuthenticationInfo(
-			AuthenticationToken authcToken) throws AuthenticationException {
-		try {
-			UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
+    /**
+     * Passo 1: Autenticando<br>
+     * Trazendo o identificador unico do usuario
+     */
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken) throws AuthenticationException {
+        try {
+            UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
 
-			Map<String, String> criterio = new HashMap<String, String>();
-			criterio.put("login", token.getUsername());
+            Map<String, String> criterio = new HashMap<String, String>();
+            criterio.put("login", token.getUsername());
 
-			UsuarioBean user = HS.searchByValueUnique(UsuarioBean.class,
-					criterio);
-			if (user != null) {
-				SimplePrincipalCollection coll = new SimplePrincipalCollection();
-				coll.add(user.getUsuario_id(), this.getName());
-				coll.add(user.getLogin(), this.getName());
-				SimpleAuthenticationInfo sai = new SimpleAuthenticationInfo(
-						coll, user.getPassword(), this.getName());
-				return sai;
-			} else {
-				throw new Exception("Usuario não encontrado");
-			}
-		} catch (HibernateException e) {
-			SecurityRealm.LOG.error("SHIRO REALM HIBERNATE", e);
-		} catch (Exception e) {
-			SecurityRealm.LOG.error("SHIRO REALM", e);
-		}
-		return null;
-	}
+            UsuarioBean user = HS.searchByValueUnique(UsuarioBean.class, criterio);
+            if (user != null) {
+                SimplePrincipalCollection coll = new SimplePrincipalCollection();
+                coll.add(user.getUsuario_id(), this.getName());
+                coll.add(user.getLogin(), this.getName());
+                SimpleAuthenticationInfo sai = new SimpleAuthenticationInfo(coll, user.getPassword(), this.getName());
+                return sai;
+            }
+            else {
+                throw new Exception("Usuario não encontrado");
+            }
+        }
+        catch (HibernateException e) {
+            SecurityRealm.LOG.error("SHIRO REALM HIBERNATE", e);
+        }
+        catch (Exception e) {
+            SecurityRealm.LOG.error("SHIRO REALM", e);
+        }
+        return null;
+    }
 
-	/**
-	 * Passo 2: Autorizando<br>
-	 * Que pode ser com mais de uma credencial
-	 */
-	@Override
-	protected AuthorizationInfo doGetAuthorizationInfo(
-			PrincipalCollection principals) {
-		SimpleAuthorizationInfo sinfo = new SimpleAuthorizationInfo();
-		for (Object pc : principals.fromRealm(this.getName())) {
-			if (pc instanceof SimplePrincipalCollection) {
-				Long id = (Long) ((SimplePrincipalCollection) pc)
-						.getPrimaryPrincipal();
+    /**
+     * Passo 2: Autorizando<br>
+     * Que pode ser com mais de uma credencial
+     */
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        SimpleAuthorizationInfo sinfo = new SimpleAuthorizationInfo();
+        for (Object pc : principals.fromRealm(this.getName())) {
+            if (pc instanceof SimplePrincipalCollection) {
+                Long id = (Long) ((SimplePrincipalCollection) pc).getPrimaryPrincipal();
 
-				Session ses = HS.getSession();
-				try {
-					Criteria crit = ses.createCriteria(UsuarioBean.class);
-					crit.add(Restrictions.eq("usuario_id", id));
-					UsuarioBean usuario = (UsuarioBean) crit.uniqueResult();
-					for (PerfilBean ln : usuario.getPerfis()) {
-						for (PermissaoBean ln3 : ln.getPermissoes()) {
-							String msg = ln3.getRecurso().getVerb() + ":"
-									+ ln3.getRecurso().getUrl() + ":"
-									+ ln3.getPrivilegio().getTp_privilegio();
-							sinfo.addStringPermission(msg);
-						}
-					}
-				} catch (Exception e) {
-					SecurityRealm.LOG.error("SHIRO PERMISSION", e);
-				} finally {
-					try {
-						ses.close();
-					} catch (Exception e3) {
-					}
-				}
-			}
-		}
-		DefaultSecurityManager m = new DefaultSecurityManager();
-		return sinfo;
-	}
+                Session ses = HS.getSession();
+                try {
+                    Criteria crit = ses.createCriteria(UsuarioBean.class);
+                    crit.add(Restrictions.idEq(id));
+                    UsuarioBean usuario = (UsuarioBean) crit.uniqueResult();
+                    for (PerfilBean ln : usuario.getPerfis()) {
+                        for (PermissaoBean ln3 : ln.getPermissoes()) {
+                            if (ln3.getPrivilegio() != null && ln3.getRecurso() != null) {
+                                String msg = ln3.getRecurso().getVerb() + ":" + ln3.getRecurso().getUrl() + ":"
+                                        + ln3.getPrivilegio().getTp_privilegio();
+                                sinfo.addStringPermission(msg);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    SecurityRealm.LOG.error("SHIRO PERMISSION", e);
+                }
+                finally {
+                    try {
+                        ses.close();
+                    }
+                    catch (Exception e3) {
+                    }
+                }
+            }
+        }
+        return sinfo;
+    }
 
-	
-	@Override
-	public void onLogout(PrincipalCollection principals) {
-		super.clearCache(principals);
-		super.onLogout(principals);
-	}
+    @Override
+    public void onLogout(PrincipalCollection principals) {
+        super.clearCache(principals);
+        super.onLogout(principals);
+    }
 
 }
