72157c9567616c0a39d3b60e819755065f6199a5
renanvcampos <renanvcampos@gmail.com>
2012-05-14 17:40:23 -0300

    dispatcher

---
+++ src/br/com/usjt/jaxrs/security/SecurityPrivate.java
@@ -0,0 +1,38 @@
+package br.com.usjt.jaxrs.security;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Anotação para informar se o recurso é travado. A anotação é opcional
+ */
+@Target (ElementType.METHOD)
+@Retention (RetentionPolicy.RUNTIME)
+@Documented
+public @interface SecurityPrivate {
+
+    /**
+     * @return privilegios
+     */
+    SecType[] permission() default SecType.SELECT;
+
+    /**
+     * Tipos de autenticacoes
+     */
+    public static enum SecType {
+        MATRICULAR,
+        /**
+         */
+        CRIARCURSO,
+        /**
+         */
+        ACESSARCURSO,
+        /**
+         */
+        SELECT,
+    }
+
+}
