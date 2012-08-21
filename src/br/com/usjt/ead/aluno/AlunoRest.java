27df6da494da72a6da445eeb4e0c31d2a403e842
renanvcampos <renanvcampos@localhost>
2012-08-20 23:30:49 -0300

    SQL + Beans

--- src/br/com/usjt/ead/aluno/AlunoRest.java
+++ src/br/com/usjt/ead/aluno/AlunoRest.java
@@ -7,10 +7,15 @@ import javax.ws.rs.Path;
 import org.jboss.resteasy.annotations.providers.jaxb.Stylesheet;
 
 import br.com.usjt.ICrud;
+import br.com.usjt.ead.contato.ContatoBean;
+import br.com.usjt.ead.contato.EnderecoBean;
+import br.com.usjt.ead.contato.TelefoneBean;
+import br.com.usjt.jaxrs.JSPAttr;
 import br.com.usjt.jaxrs.MediaTypeMore;
 import br.com.usjt.jaxrs.security.SecurityPrivate;
 import br.com.usjt.jaxrs.security.SecurityPrivate.Entidade;
 import br.com.usjt.jaxrs.security.SecurityPrivate.SecType;
+import br.com.usjt.util.CryptoXFacade;
 
 @Path("/aluno")
 public class AlunoRest implements ICrud
@@ -59,8 +64,21 @@ public class AlunoRest implements ICrud
     @Stylesheet(href = "/read.jsp", type = MediaTypeMore.APP_JSP)
     @SecurityPrivate(permission = SecType.CRIAR, entity = Entidade.DUMMY)
     public void create() {
-        // TODO Auto-generated method stub
-
+        JSPAttr j = new JSPAttr();
+        AlunoBean b = new AlunoBean();
+        b.setEmail(j.getParameter("txtEmail"));
+        b.setSenha(CryptoXFacade.cryptox.crypt(j.getParameter("txtSenha")));
+        b.setCpf(Long.parseLong(j.getParameter("txtCPF").replaceAll("[^0-9]", "")));
+        EnderecoBean end = new EnderecoBean();
+        end.setCep(Integer.parseInt(j.getParameter("txtCEP").replaceAll("[^0-9]", "")));
+        end.setLogradouro(j.getParameter("txtEndereco"));
+        end.setBairro(j.getParameter("txtBairro"));
+        b.setEndereco(end);
+        ContatoBean contato = new ContatoBean();
+        TelefoneBean phone = new TelefoneBean();
+        phone.setDdd(Integer.parseInt(j.getParameter("txtDDD")));
+        phone.setTelefone(Long.parseLong(j.getParameter("txtTelefone")));
+        contato.getTelefones().add(phone);
     }
 
     @Override
